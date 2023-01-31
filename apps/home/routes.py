5a29dde293f08139d585384d328cfe94e142d9c9
Jeffrey Sorsby <jeffrey@sorsby.mx>
2023-01-31 00:29:24 -0800

    reddit_callback and post.html updates

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -7,13 +7,62 @@ from apps.home import blueprint
 from flask import render_template, request
 from flask_login import login_required
 from jinja2 import TemplateNotFound
+import requests
+import os
 
 
 @blueprint.route('/index')
 @login_required
 def index():
+    url = "http://host.docker.internal:8000/reddit/submissions?query=wd-40&limit=100"
+    
+    payload={}
+    headers = {
+      'Accept': 'application/json',
+      'Accept-Encoding': 'gzip, deflate',
+      'Host': 'host.docker.internal:8000',
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.2 Safari/605.1.15',
+      'Accept-Language': 'en-US,en;q=0.9',
+      'Referer': 'http://host.docker.internal:8000/docs',
+      'Connection': 'keep-alive'
+    }
+    
+    response = requests.request("GET", url, headers=headers, data=payload)
+    
+    posts = response.json()['results']
+    return render_template('home/index.html', segment='index', posts=posts)
 
-    return render_template('home/index.html', segment='index')
+@blueprint.route('/post')
+@login_required
+def post_detail():
+    _id = request.args.get('_id')
+    url = "http://host.docker.internal:8000/reddit/submissions?_id={}".format(_id)
+    
+    payload={}
+    headers = {
+      'Accept': 'application/json',
+      'Accept-Encoding': 'gzip, deflate',
+      'Host': 'host.docker.internal:8000',
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.2 Safari/605.1.15',
+      'Accept-Language': 'en-US,en;q=0.9',
+      'Referer': 'http://host.docker.internal:8000/docs',
+      'Connection': 'keep-alive'
+    }
+    
+    response = requests.request("GET", url, headers=headers, data=payload)
+    
+    post = response.json()['results'][0]
+    return render_template('home/post.html', segment='index', post=post)
+
+
+@blueprint.route('/reddit_callback')
+@login_required
+def reddit_callback():
+    # extract code and state from query string
+    username = request.args.get('username')
+    read_only = request.args.get('read_only')
+    
+    return render_template('home/reddit_callback.html', username=username, read_only=read_only)
 
 
 @blueprint.route('/<template>')
